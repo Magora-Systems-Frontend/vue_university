2cb671669f00daed3efc5df4d53be4901df5d7e9
Shurumov Aleksei <Shurumov@magora-systems.com>
2019-10-25 10:24:10 +0700

    [+]FRONTEND-269: changed scripts package.json

--- src/entry-client.js
+++ src/entry-client.js
@@ -3,17 +3,11 @@ import { loadAsyncComponents } from '@akryum/vue-cli-plugin-ssr/client'
 import { createApp } from './main'
 
 createApp({
-  async beforeApp ({
-                     router
-                   }) {
+  async beforeApp ({ router }) {
     await loadAsyncComponents({ router })
   },
 
-  afterApp ({
-              app,
-              router,
-              store,
-            }) {
+  afterApp ({ app, router, store, }) {
     store.replaceState(window.__INITIAL_STATE__);
     router.onReady(() => {
       app.$mount('#app')
