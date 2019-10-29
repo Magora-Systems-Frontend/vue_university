9123ebbd5dca5d6df86d150d497fa92ac83006a3
Shurumov Aleksei <Shurumov@magora-systems.com>
2019-10-29 11:55:29 +0700

    [*]FRONTEND-292: added radiobutton group

--- src/pages/TestPage/TestPage.vue
+++ src/pages/TestPage/TestPage.vue
@@ -1,18 +1,27 @@
 <template>
+  <div style="padding: 20px">
     <Checkbox
             label="Checkbox label"
             name="checkbox_name"
             :isChecked="true"
             error="ААААААААААААА"
     />
+    <RadioButton
+            :data="[{ title: 'One', value: 1 }, { title: 'Two', value: 2 }]"
+            :columnsCount="1"
+            error="AAAAA"
+    />
+  </div>
 </template>
 
 <script>
   import Checkbox from 'components/FormElements/Checkbox/Checkbox'
+  import RadioButton from 'components/FormElements/RadioButton'
 
   export default {
     components: {
-      Checkbox
+      Checkbox,
+      RadioButton
     },
   }
 </script>
